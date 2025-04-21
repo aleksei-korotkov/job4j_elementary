367906f7218216ac5371fa5a32c2c5b8786383e8
job4j_elementary <aleksei-korotkov@yandex.ru>
2025-04-22 02:34:52 +0300

    1. Модульные тесты [#505277]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,10 +2,10 @@ package ru.job4j.converter;
 
 public class Converter {
     public static double rubleToEuro(double value) {
-        return value / 90;
+        return (value / 90);
     }
 
     public static double rubleToDollar(double value) {
-        return value / 75;
+        return (value / 75);
     }
 }
