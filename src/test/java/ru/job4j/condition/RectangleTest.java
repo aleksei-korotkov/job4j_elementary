5f2585d816aa4e7de8135e7ca96453d04f5b79d2
job4j_elementary <aleksei-korotkov@yandex.ru>
2025-04-27 04:16:10 +0300

    Тестирование Класса Вычисления Диагонали Прямоугольника [#505281]

---
+++ src/test/java/ru/job4j/condition/RectangleTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class RectangleTest {
+
+    @Test
+    void whenLength3AndWidth4ThenDiagonal5() {
+        double length = 3;
+        double width = 4;
+        double expected = 5.0;
+        double output = Rectangle.diagonal(length, width);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenLength5AndWidth12ThenDiagonal13() {
+        double length = 5;
+        double width = 12;
+        double expected = 13.0;
+        double output = Rectangle.diagonal(length, width);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenLength7AndWidth24ThenDiagonal25() {
+        double length = 7;
+        double width = 24;
+        double expected = 25.0;
+        double output = Rectangle.diagonal(length, width);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenLength2AndWidth4ThenDiagonal4Dot47() {
+        double length = 2;
+        double width = 4;
+        double expected = 4.47;
+        double output = Rectangle.diagonal(length, width);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenLength13AndWidth14ThenDiagonal19Dot10() {
+        double length = 13;
+        double width = 14;
+        double expected = 19.10;
+        double output = Rectangle.diagonal(length, width);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenLength55AndWidth125ThenDiagonal136Dot56 () {
+        double length = 55;
+        double width = 125;
+        double expected = 136.56;
+        double output = Rectangle.diagonal(length, width);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
