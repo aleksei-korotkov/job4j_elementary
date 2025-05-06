0eb5132c40fc3d8d731d3783e777031bfe4f7375
job4j_elementary <aleksei-korotkov@yandex.ru>
2025-05-06 12:18:21 +0300

    Цикл do-while [#505216]

---
+++ src/main/java/ru/job4j/loop/Savings.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Savings {
+
+    public static int years(double goal, double annualDeposit, double percent) {
+        int years = 0;
+        double total = 0;
+        do {
+            double percentSum = total * (percent / 100);
+            total = total + percentSum;
+            total = total + annualDeposit;
+            years++;
+        } while (total < goal);
+        return years;
+    }
+}
