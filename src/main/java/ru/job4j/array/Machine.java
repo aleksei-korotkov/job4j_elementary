179826d966b827e427731f6b93a40d275d400ba3
job4j_elementary <aleksei-korotkov@yandex.ru>
2025-05-30 02:50:13 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#505250 #544611]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,11 +8,11 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] result = new int[100];
         int size = 0;
-        int remains = money - price;
-        for (int index = 0; index < coins.length; index++) {
-            while (remains >= coins[index]) {
-                remains = remains - coins[index];
-                result[size] = coins[index];
+        money -= price;
+        for (int coin : coins) {
+            while (money >= coin) {
+                money -= coin;
+                result[size] = coin;
                 size++;
             }
         }
--- src/test/java/ru/job4j/array/MachineTest.java
+++ src/test/java/ru/job4j/array/MachineTest.java
@@ -40,4 +40,5 @@ class MachineTest {
         int[] expected = {10, 5, 2, 1};
         assertThat(result).containsExactly(expected);
     }
+
 }
