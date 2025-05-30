7dfca68820f9f80c826263551986f9a229c5e45c
job4j_elementary <aleksei-korotkov@yandex.ru>
2025-05-30 12:46:45 +0300

    6.8. Метод двух указателей [#505249 #544608]

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -15,4 +15,23 @@ public class TwoNumberSum {
         }
         return new int[0];
     }
-}
+
+    public static int[] getIndexes2(int[] array, int target) {
+        int i = 0;
+        int j = i + 1;
+        while (i < array.length - 1) {
+                if ((array[i] + array[j]) == target) {
+                    int[] arrayOfIndex = new int[2];
+                    arrayOfIndex[0] = i;
+                    arrayOfIndex[1] = j;
+                    return  arrayOfIndex;
+                }
+                j++;
+                if (j >= array.length) {
+                    i++;
+                    j = i + 1;
+                }
+            }
+            return new int[0];
+        }
+ }
--- src/test/java/ru/job4j/array/TwoNumberSumTest.java
+++ src/test/java/ru/job4j/array/TwoNumberSumTest.java
@@ -40,4 +40,40 @@ class TwoNumberSumTest {
         int[] expected = {2, 4};
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenTwoEqualsNumbersYesTargetForGetIndexes2() {
+        int[] array = {5, 5};
+        int target = 10;
+        int[] result = TwoNumberSum.getIndexes2(array, target);
+        int[] expected = {0, 1};
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenTwoEqualsNumbersNoTargetForGetIndexes2() {
+        int[] array = {5, 5};
+        int target = 12;
+        int[] result = TwoNumberSum.getIndexes2(array, target);
+        int[] expected = {};
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenWithNegativeNumbersYesTargetForGetIndexes2() {
+        int[] array = {-7, -5, 0, 5, 8, 12};
+        int target = 3;
+        int[] result = TwoNumberSum.getIndexes2(array, target);
+        int[] expected = {1, 4};
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenWithoutNegativeNumbersYesTargetForGetIndexes2() {
+        int[] array = {0, 2, 5, 8, 10, 12};
+        int target = 15;
+        int[] result = TwoNumberSum.getIndexes2(array, target);
+        int[] expected = {2, 4};
+        assertThat(result).isEqualTo(expected);
+    }
 }
